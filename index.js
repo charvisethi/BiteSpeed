5ddf29b5ac00f8456a2fa18bfb7f1723d64fc547
charvi <charvipm@gmail.com>
2025-02-11 00:49:04 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const dbURI = process.env.MONGO_URI; // Get the URI from the environment variable
 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("MongoDB connection error:", err));
 const app = express();
 const PORT = 4000;
 
